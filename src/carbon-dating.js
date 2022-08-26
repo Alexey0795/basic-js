4079c8ebc6bae3d58a9ab9ec53baaa0cc1ef2e66
alu-wp <no-email@none>
2022-08-26 16:52:30 +0300

    done: carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,6 +2,7 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
+const RATE = 0.693 / HALF_LIFE_PERIOD;
 
 /**
  * Determine the age of archeological find by using
@@ -17,9 +18,15 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (!(typeof sampleActivity === 'string' ||
+    sampleActivity instanceof String)) return false;
+  if (isNaN(sampleActivity)) return false;
+  if (sampleActivity > MODERN_ACTIVITY) return false;
+  if (sampleActivity <= 0) return false;
+
+  let result = Math.ceil(Math.log(MODERN_ACTIVITY / parseFloat(sampleActivity)) / RATE);
+  return result
 }
 
 module.exports = {
